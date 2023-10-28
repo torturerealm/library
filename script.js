76e77c6e7d4491a7ee370526288fa8bb841e20de
torturerealm <76659489+torturerealm@users.noreply.github.com>
2023-10-28 20:42:20 +0800

    Add array and object constructor in js

---
+++ script.js
@@ -0,0 +1,15 @@
+const myLibrary = [];
+
+function Book() {
+    //constructor
+}
+
+function addBookToLibrary() {
+    // do stuff
+}
+
+function displayEachBook() {
+    // loop through array
+    // display each book
+}
+
