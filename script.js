ac7149782fa53c537638ecdd71155a989786dddd
torturerealm <76659489+torturerealm@users.noreply.github.com>
2023-10-29 19:00:08 +0800

    Style card and set grid in display
    For better visuals and arrangement

--- script.js
+++ script.js
@@ -22,13 +22,15 @@ function displayEachBook() {
         
         const card = document.createElement("div");
         card.classList.add("card");
-        card.textContent = `${item.title} by ${item.author}, ${item.pages}, Read: ${item.status}`;
+        card.textContent = `${item.title} by ${item.author}, ${item.page}, Read: ${item.status}`;
         display.appendChild(card);
     });
 }
 
 const book1 = new Book("Hello", "Hi", 23, true);
 const book2 = new Book("Thunderclap", "PE Dekebna", 344, false);
+myLibrary.push(book1);
+myLibrary.push(book2);
 myLibrary.push(book1);
 myLibrary.push(book2);
 displayEachBook();
