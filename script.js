7d1fbba337a42d86d82c2f964d9fdbc9c2f9da09
torturerealm <76659489+torturerealm@users.noreply.github.com>
2023-10-29 18:40:00 +0800

    Update displayEachBook function
    So that each item/book in the myLibrary array could be displayed in cards.

--- script.js
+++ script.js
@@ -1,20 +1,36 @@
 const myLibrary = [];
 
 function Book(title, author, page, status) {
-    this.title = title;
-    this.author = author;
-    this.page = page;
+    this.title = title,
+    this.author = author,
+    this.page = page,
     this.status = function() {
-        status ? "read": "not yet read";
+        if (status) return "read"
+        else return "not yet read";
     }
 }
 
 function addBookToLibrary() {
-    // do stuff
+    // push new object inside array
 }
 
 function displayEachBook() {
     // loop through array
     // display each book
+    myLibrary.forEach(item => {
+        const display = document.querySelector(".book-display");
+        
+        const card = document.createElement("div");
+        card.classList.add("card");
+        card.textContent = `${item.title} by ${item.author}, ${item.pages}, Read: ${item.status}`;
+        display.appendChild(card);
+    });
 }
+
+const book1 = new Book("Hello", "Hi", 23, true);
+const book2 = new Book("Thunderclap", "PE Dekebna", 344, false);
+myLibrary.push(book1);
+myLibrary.push(book2);
+displayEachBook();
+
 
